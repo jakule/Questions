02ec31b9759d5cbae7736fb663a2c465d2b3d89d
adam <pajak.adam.emil@gmail.com>
2019-01-21 23:10:06 +0100

    Data

--- Questions.py
+++ Questions.py
@@ -5,8 +5,7 @@ import collections
 
 
 class Question:
-    def __init__(self, id, contents, correct_answers):
-        self.id = id
+    def __init__(self, contents, correct_answer):
         self.contents = contents
         self.possible_answers = []
         self.correct_answer = correct_answer
@@ -56,26 +55,41 @@ def fill_data(lines):
                     print(lines[i][j + 7:].replace(' ', '').replace(',', ''))
                 break
 
-
-
-if __name__ == '__main__':
-
-
+def import_data_from_file():
     pdf_file = open('123.pdf', 'rb')
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
     number_of_pages = read_pdf.getNumPages()
     c = number_of_pages
-    for page_now in range(1,5):
+    lines = ['']
+    for page_now in range(1, 5):
+        # for page_now in range(read_pdf.getNumPages()):
         page = read_pdf.getPage(page_now)
         page_content = page.extractText()
+        lines = lines + page_content.splitlines()
+    return(lines)
 
-        lines = page_content.splitlines()
-        fill_data(lines)
-        #print (page_content)
+if __name__ == '__main__':
+
+
+    lines = import_data_from_file()
+    temp_lines= []
+    for line in lines: #every line before Question 1 contains something
+        temp_line = line.replace(' ','')
+        if temp_line.startswith('Question'):
+            break
+        else:
+            if temp_line != '':
+                temp_lines.append(line)
+    #for line in temp_lines: # lines before Question contains something
+
+    print(temp_lines, len(temp_lines))
+    fill_data(lines)
+
+
+
 
 
     #read_name_file()
-    #import_data_from_file()
     #delete_numer_page()
     #delete_section_page_footer()
     #divide_data_into_parts()
