f4d84642f950864b7d4598958c15e10a35e99699
adam <pajak.adam.emil@gmail.com>
2019-01-22 22:14:02 +0100

    Data

--- Questions.py
+++ Questions.py
@@ -21,36 +21,36 @@ def fill_data(lines):
                 break
             elif lines[i][j:j + 2] == 'A.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
             elif lines[i][j:j + 2] == 'B.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
 
             elif lines[i][j:j + 2] == 'C.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
             elif lines[i][j:j + 2] == 'D.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
 
             elif lines[i][j:j + 2] == 'E.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
             elif lines[i][j:j + 2] == 'F.':
                 flag = 1
-                print(lines[i])
+                #print(lines[i])
                 break
             elif lines[i][j:j + 7] == 'Answer:':
                 flag = 1
                 print(lines[i][j:j + 7])
 
                 if lines[i][j + 7:].replace(' ', '').replace(',', '') == '':
-                    print('Withonut correct answer')
+                    print('Withonut correct answer', lines[i],lines[i+1],lines[i+2],lines[i+3])
                 else:
                     print(lines[i][j + 7:].replace(' ', '').replace(',', ''))
                 break
@@ -58,32 +58,66 @@ def fill_data(lines):
 def import_data_from_file():
     pdf_file = open('123.pdf', 'rb')
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
-    number_of_pages = read_pdf.getNumPages()
-    c = number_of_pages
     lines = ['']
-    for page_now in range(1, 5):
-        # for page_now in range(read_pdf.getNumPages()):
-        page = read_pdf.getPage(page_now)
-        page_content = page.extractText()
-        lines = lines + page_content.splitlines()
+    for page_now in range(1, 3):
+    #for page_now in range(read_pdf.getNumPages()):
+        page_content = read_pdf.getPage(page_now).extractText()
+        lines.extend(page_content.splitlines())
     return(lines)
 
-if __name__ == '__main__':
+def remove_page_number(lines):
+    lines_now = []
+    number_page_inside = 1
+    for line in lines:
+        temp_line = line.replace(' ', '')
+        if temp_line == str(number_page_inside):
+            number_page_inside += 1
+            continue
+        else:
+            lines_now.append(line)
+    return(lines_now)
 
-
-    lines = import_data_from_file()
-    temp_lines= []
-    for line in lines: #every line before Question 1 contains something
-        temp_line = line.replace(' ','')
+def remove_section_page_footer(lines):
+    temp_lines = []
+    for line in lines:  # lines before Question 1 contains something
+        temp_line = line.replace(' ', '')
         if temp_line.startswith('Question'):
             break
         else:
             if temp_line != '':
                 temp_lines.append(line)
+    return (delete_page_footer(lines, temp_lines))
+
+
+def delete_page_footer(lines,temp_lines):
+    lines_now = []
+    flag = 1
+    for line in lines:
+        for wrong_lines in temp_lines:
+            if line.replace(' ', '') == wrong_lines.replace(' ', ''):
+                flag = 0
+        if flag == 1:
+            lines_now.append(line)
+        else:
+            flag = 1
+    return(lines_now)
+
+
+
+if __name__ == '__main__':
+
+
+    lines = import_data_from_file()
+    lines = remove_page_number(lines)
+    lines = remove_section_page_footer(lines)
+    fill_data(lines)
+
+
+    #print(lines_now)
     #for line in temp_lines: # lines before Question contains something
 
-    print(temp_lines, len(temp_lines))
-    fill_data(lines)
+    #print(temp_lines, len(temp_lines))
+    #fill_data(lines)
 
 
 
@@ -96,3 +130,9 @@ if __name__ == '__main__':
     #allocate_data_to_class()
     #randomize_numer_question()
     #get_a_question()
+'''
+
+
+
+
+'''
