4dd10aa7dfae6b773ea56f689ed1496d27467518
adam <pajak.adam.emil@gmail.com>
2019-01-25 20:18:33 +0100

    Data

--- Questions.py
+++ Questions.py
@@ -4,6 +4,7 @@ import PyPDF2
 import time
 import argparse
 
+TEMPORARY_MEMORY = [[] for _ in range(24)]
 
 class Question:
     def __init__(self, contents, correct_answer):
@@ -11,60 +12,41 @@ class Question:
         self.possible_answers = []
         self.correct_answer = correct_answer
 
-def fill_data(lines):
-    flag =0
-    for i in range(len(lines)):
-        for j in range(2):
-            if lines[i][j:j + 8] == 'Question':
-                flag = 1
-                print(lines[i])
-                break
-            elif lines[i][j:j + 2] == 'A.':
-                flag = 1
-                #print(lines[i])
-                break
-            elif lines[i][j:j + 2] == 'B.':
-                flag = 1
-                #print(lines[i])
-                break
+def fill_data(line,flag):
+    if (flag != -1) and flag != 0:
+        TEMPORARY_MEMORY[flag].append(line)
+    if flag == 0 and not line.replace(' ', '') == '':
+        TEMPORARY_MEMORY[flag].append(line.replace(' ', ''))
 
-            elif lines[i][j:j + 2] == 'C.':
-                flag = 1
-                #print(lines[i])
-                break
-            elif lines[i][j:j + 2] == 'D.':
-                flag = 1
-                #print(lines[i])
-                break
 
-            elif lines[i][j:j + 2] == 'E.':
-                flag = 1
-                #print(lines[i])
-                break
-            elif lines[i][j:j + 2] == 'F.':
-                flag = 1
-                #print(lines[i])
-                break
-            elif lines[i][j:j + 7] == 'Answer:':
-                flag = 1
-                print(lines[i][j:j + 7])
+def sort_data(lines):#flag =-2 at start, flag = -1 for other flag = 0 for answers, flag = 1 for Question X, flag =2,3,4,5... for A,B,C,D...
+    flag = -2
+    for line in lines:
+        temp_line = line.replace(' ', '')
+        if temp_line.startswith('Question'):
+            flag = 1
+        if temp_line.startswith('A.') or temp_line.startswith('B.') or temp_line.startswith('C.') or temp_line.startswith('D.') or temp_line.startswith('E.') or temp_line.startswith('F.') or temp_line.startswith('G.'):
+            flag += 1
+        if temp_line.startswith('Answer:'):
+            flag = 0
 
-                if lines[i][j + 7:].replace(' ', '').replace(',', '') == '':
-                    print('Withonut correct answer', lines[i],lines[i+1],lines[i+2],lines[i+3])
-                else:
-                    print(lines[i][j + 7:].replace(' ', '').replace(',', ''))
-                break
+        fill_data(line,flag)
+        #print(flag, line)
+    print(''.join(TEMPORARY_MEMORY[0]))
+
+
 
 def import_data_from_file():
     pdf_file = open('123.pdf', 'rb')
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
     lines = []
-    for page_now in range(1, 3):
-    #for page_now in range(read_pdf.getNumPages()):
+    for page_now in range(1, 25):
+        # for page_now in range(read_pdf.getNumPages()):
         page_content = read_pdf.getPage(page_now).extractText()
         lines.extend(page_content.splitlines())
-    #lines.extend(a for page_now in range(read_pdf.getNumPages()) for a in read_pdf.getPage(page_now).extractText().splitlines())
-    return(lines)
+    # lines.extend(a for page_now in range(read_pdf.getNumPages()) for a in read_pdf.getPage(page_now).extractText().splitlines())
+    return lines
+
 
 def remove_page_number(lines):
     lines_now = []
@@ -77,10 +59,12 @@ def remove_page_number(lines):
             continue
         else:
             lines_now.append(line)
-    return (lines_now)
+    return lines_now
+
 
 def end_of_loop():
     raise StopIteration
+
 
 def remove_section_page_footer(lines):
     temp_lines = []
@@ -91,10 +75,11 @@ def remove_section_page_footer(lines):
         else:
             if temp_line != '':
                 temp_lines.append(line)
-    #temp_lines= list(end_of_loop() if line.replace(' ', '').startswith('Question') else line for line in lines)
-    return (delete_page_footer(lines, temp_lines))
+    # temp_lines= list(end_of_loop() if line.replace(' ', '').startswith('Question') else line for line in lines)
+    return delete_page_footer(lines, temp_lines)
 
-def delete_page_footer(lines,temp_lines):
+
+def delete_page_footer(lines, temp_lines):
     lines_now = []
     flag = True
     for line in lines:
@@ -105,7 +90,12 @@ def delete_page_footer(lines,temp_lines):
             lines_now.append(line)
         else:
             flag = True
-    return(lines_now)
+    return lines_now
+
+def delete_empy_lines_at_beginning_of_the_document(lines):
+    while not lines[0].replace(' ', '').startswith('Question'):
+        del lines[0]
+    return lines
 
 
 if __name__ == '__main__':
@@ -122,14 +112,14 @@ if __name__ == '__main__':
     else:
         print('I can not find it')
 
-
     lines = import_data_from_file()
     lines = remove_page_number(lines)
     lines = remove_section_page_footer(lines)
-    fill_data(lines)
+    lines = delete_empy_lines_at_beginning_of_the_document(lines)
+    sort_data(lines)
 
 
-#divide_data_into_parts()
-#allocate_data_to_class()
-#randomize_numer_question()
-#get_a_question()
+# divide_data_into_parts()
+# allocate_data_to_class()
+# randomize_numer_question()
+# get_a_question()
